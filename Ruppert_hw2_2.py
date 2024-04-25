7e8c5c1eae311c6c1b882b29cc216c0336ec3186
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-04-25 16:20:53 -0400

    Update Ruppert_hw2_2.py

--- Ruppert_hw2_2.py
+++ Ruppert_hw2_2.py
@@ -8,17 +8,23 @@ Created Date: 25Apr24
 
 import sys
 
+# Global variables
 missing_chars = []
 special_char = False
 digit = False
 uppercase = False
 lowercase = False
     
-
+# Script intro
 print('Passwords are great, but you should be safe with them.\nPlease make sure your password includes special characters, digits, uppercase letters, and lowercase letters.')
+
+# Begin while loop to try for error handling.
 while True:
     try:
+        # User input
         user_input = input('Enter your password, or type Q to quit: ')
+        
+        # Look for character's characteristics to see if they validate
         for char in user_input:
             if not char.isalnum():
                 special_char = True
@@ -30,16 +36,20 @@ while True:
                 lowercase = True
         validation = special_char and digit and uppercase and lowercase
         
+        # Verify if user wants to quit
         if user_input.lower() == 'q':
             sys.exit(1)
         
+        # Verify if password is long enough
         elif len(user_input) < 14:
             print('\nYour password must be at least 14 characters long.')
             sys.exit(1)
         
+        # Pull validation to see if its True; if True, break to end
         elif validation:
             break
         
+        # Look through and build list of missing character sets; notify user of missing sets
         else:
             if not special_char:
                 missing_chars.append('special character')
@@ -51,10 +61,11 @@ while True:
                 missing_chars.append('lowercase letter')
             print(f"You are missing {len(missing_chars)} character sets. Please add a {', '.join(missing_chars)} to your password.")
             sys.exit(1)
-            
+    
+    # input handling        
     except KeyboardInterrupt as e:
         sys.exit(1)
             
-            
+# When successful, notify user and exit            
 print(f'Your password, \'{user_input}\', is secure.')
 sys.exit(1)        
