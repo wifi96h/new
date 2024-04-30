b9ece7a72668a6f9d45408dcc37acc61e305c7ed
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-04-30 16:18:24 -0400

    Update Ruppert_hw4.py

--- Ruppert_hw4.py
+++ Ruppert_hw4.py
@@ -18,61 +18,43 @@ Create program that:
 
 # sys.argv
 import sys
+import re
 
+with open(sys.argv[1]) as file:
+    text = file.read()
 
+# removes special characters
+clean_text = re.sub(r"[^\w\s]", '', text)
 
-def file_importer():
-    # readlines file to put into lists
-    with open(sys.argv[-1]) as file:
-        file_content = file.readlines()
-    
-        # strip out '\n' characters and add to modified list
-        mod_file_content = []
-        for i in file_content:
-            mod_file_content.append(i.strip())
-        
-        #count words in file
-        word_count = len(mod_file_content)
-        return mod_file_content, word_count
+# places clean_text into list
+text_list = clean_text.split()
+word_count = len(text_list)
 
-        
-def count_char(mod_file_content):
+# count characters in string that is not white space
+freq = {}
+for char in clean_text:
+    if char.isalpha():
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+top_letter = max(freq, key=freq.get)
 
-    # count characters in modified list that is not white space characters
-    largest_num = 0
-    for char in mod_file_content:
-        if char.isalnum():
-            if mod_file_content.count(char) > largest_num:
-                largest_num = mod_file_content.count(char)
-        
-    # print what most common letter is and how many times it occurs; 
-    print(f'{char} is the most common letter. It occurs {largest_num} times.')
+print(f'{top_letter} is the most common letter. It occurs {clean_text.count(top_letter)} times.')
 
+# counts number of times 'the' occurs in file
+the_count = 0
+for word in map(str.lower, text_list):
+    if word == 'the':
+        the_count += 1
 
-def count_the(mod_file_content, word_count):
-    # count the number of times 'the' occurs
-    the_count = 0
-    for word in (mod_file_content):
-        if word.lower() == 'the':
-            the_count += 1
+# determine the percentage of the words in file == 'the'
+the_perct = the_count / word_count
 
-    # determine the percentage of the words in file == 'the'
-    the_perct = the_count / word_count
+# print times 'the' occurs in text
+print(f'\'The\' is {the_count} of {word_count} or {the_perct:.2%}')
 
-    # print times 'the' occurs in text
-    print(f'\'The\' is {the_count} of {word_count} or {the_perct:.2%}')
-
-
-def new_file_writer(mod_file_content):
-    # write first ten words of the file to a new file named 'ten_words.txt'
-    with open('ten_words.txt','w') as ten_words:
-        ten_words.write(mod_file_content[0:10])
-
-
-def __main__():
-    mod_file_content, word_count = file_importer()
-    count_char(mod_file_content)
-    count_the(mod_file_content, word_count)
-    new_file_writer(mod_file_content)
-
-__main__()
+# writes first ten words of file to a new file named 'ten_words.txt'
+with open('ten_words.txt','w') as ten_words:
+    writing = ' '.join(text_list[:10])
+    ten_words.write(writing)
