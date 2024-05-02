c557b04e1b952d3f9e188bd65550280e95d42fb3
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-05-02 09:57:18 -0400

    Create Ruppert_hw6.py

---
+++ Ruppert_hw6.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+'''
+Author: WO1 Tony Ruppert
+Class: 24-002 WOBC
+Py_Version: 3.8
+Created_Date: 2May24
+'''
+
+'''
+Create a class named Motorcycle with a speedup function, slowdown function, and a stop function.
+Create the speedup and slowdown functions to accept a number and change the speed by that amount.
+The stop function must set the speed back to 0.
+Use the __str__() method to state the current speed.
+'''
+
+class Motorcycle():
+  def __init__(self, speedup, slowdown, stop_moving):
+    self.speedup = speedup
+    self.slowdown = slowdown
+    self.stop_moving = stop_moving
+  def speedup():
+    pass
+  def slowdown():
+    pass
+  def stop_moving():
+    pass
+
+while True:
+  try:
+    change = input(
