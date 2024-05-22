07c6476317f55e8a68b46fd7c5c7bb204aa8ea02
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-05-22 18:15:36 -0400

    Update Day6_windows_process_validity.ps1

--- CCTC/OS/Day6_windows_process_validity.ps1
+++ CCTC/OS/Day6_windows_process_validity.ps1
@@ -1,1 +1,10 @@
+# There is malware on the system that is named similarly to a legitimate Windows executable. There is a .dll in the folder that the malware runs from. The flag is the name of the .dll.
+get-process | select name, id, path | sort name
 
+# You notice that there is an annoying pop up happening regularly. Investigate the process causing it. The flag is the name of the executable.
+search task scheduler
+
+# Determine what is sending out a SYN_SENT message. The flag is the name of the executable.
+.\tcpview.exe # opens tcpview gui
+
+# Malware uses names of legit processes to obfuscate itself. Give the flag located in Kerberos’ registry subkey.
