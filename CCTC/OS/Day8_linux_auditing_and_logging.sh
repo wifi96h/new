21467192af23c567741ffc0b4dd379f12cd60a44
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-05-28 16:39:26 -0400

    Update Day8_linux_auditing_and_logging.sh

--- CCTC/OS/Day8_linux_auditing_and_logging.sh
+++ CCTC/OS/Day8_linux_auditing_and_logging.sh
@@ -6,3 +6,7 @@ xpath -q -e '//address/@addr | //port/@portid' /home/garviel/output.xml | md5sum
 
 # Hash the pretty-printed file with md5sum for the flag
 jq . conn.log | md5sum
+
+# Use jq to locate and count the unique originating endpoint IP addresses in the file. Enter the number of unique originating IP addresses as the flag.
+jq '."id.orig_h"' conn.log | sort -u | wc -l
+
