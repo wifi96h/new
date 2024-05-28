b4e884bbc89e4aea3e59176542873b5dec527f0c
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-05-28 09:10:51 -0400

    Update Day7_linux_process_validity.sh

--- CCTC/OS/Day7_linux_process_validity.sh
+++ CCTC/OS/Day7_linux_process_validity.sh
@@ -3,3 +3,6 @@ ps --ppid 2
 
 # htop works better than top for in depth process analysis
 
+# Locate the strange open port on the SysV system. Identify how the process persists between reboots.
+nc -ano
+cat /etc/inittab
