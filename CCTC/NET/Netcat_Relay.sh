57be2b66bcc7a57e4890eb703777adf11eebbdb7
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-06-12 13:40:35 -0400

    Update Netcat_Relay.sh

--- CCTC/NET/Netcat_Relay.sh
+++ CCTC/NET/Netcat_Relay.sh
@@ -1,1 +1,27 @@
+T1
+Hostname: INTERNET_HOST
+External IP: 10.50.XXX.XXX (ALREADY PROVIDED) Internal IP: 10.10.0.40 (ALREADY PROVIDED) (accessed via FLOAT IP)
+creds: (ALREADY PROVIDED)
+Action: Successfully transfer file data between hosts via Netcat
 
+T2
+Hostname: BLUE_HOST-4
+IP: 172.16.82.115
+creds: (NONE)
+Action: Successfully transfer files from this host using Netcat
+
+RELAY
+Hostname: BLUE_INT_DMZ_HOST-1
+IP: 172.16.40.10
+creds: (ALREADY PROVIDED)
+Action: Successfully transfer file data between hosts via Netcat
+
+T1 > Relay > T2:file
+
+1. T1 C -> S Relay S <- C T2
+
+2. T1 C -> S Relay S <- C T2
+
+3. T1 C -> S Relay C -> S T2
+
+4. T1 C -> S Relay C -> S T2
