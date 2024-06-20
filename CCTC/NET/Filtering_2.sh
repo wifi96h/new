f825456a9258debb9acdb84db2d7ae413ee9246d
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-06-20 10:44:04 -0400

    Update Filtering_2.sh

--- CCTC/NET/Filtering_2.sh
+++ CCTC/NET/Filtering_2.sh
@@ -8,6 +8,8 @@ sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate NEW,ESTABLISHED
 
 # Change the Default Policy in the Filter Table for the INPUT, OUTPUT, and FORWARD chains to DROP
 sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT DROP
+sudo iptables -P FORWARD DROP
 
 # Allow ping (ICMP) requests (and reply) to and from the Pivot.
 sudo iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
@@ -24,3 +26,35 @@ sudo iptables -A OUTPUT -p udp -m multiport --ports 4444,6579 -j ACCEPT
 # Allow New and Established traffic to/from via HTTP
 sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+netcat -l -p 9001
+
+--or--
+# Allow New and Established traffic to/from via SSH, TELNET, and RDP
+sudo iptables -A INPUT -p tcp -m multiport --ports 22,23,3389 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -m multiport --ports 22,23,3389 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+# Change the Default Policy in the Filter Table for the INPUT, OUTPUT, and FORWARD chains to DROP
+sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT DROP
+sudo iptables -P FORWARD DROP
+
+# Allow ping (ICMP) requests (and reply) to and from the Pivot.
+sudo iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
+sudo iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT
+sudo iptables -A OUTPUT -p icmp --icmp-type 8 -j ACCEPT
+sudo iptables -A OUTPUT -p icmp --icmp-type 0 -j ACCEPT
+
+# Allow ports 6579 and 4444 for both udp and tcp traffic
+sudo iptables -A INPUT -p tcp -m multiport --ports 4444,6579 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -m multiport --ports 4444,6579 -j ACCEPT
+sudo iptables -A INPUT -p udp -m multiport --ports 4444,6579 -j ACCEPT
+sudo iptables -A OUTPUT -p udp -m multiport --ports 4444,6579 -j ACCEPT
+
+# Allow New and Established traffic to/from via HTTP
+sudo iptables -A INPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+netcat -l -p 9001
+-------------------------------------------------------
+
