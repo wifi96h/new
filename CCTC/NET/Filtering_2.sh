d7dc73f653bb19d8b80979b18a607492cf6e0c4e
ruppertaj <93789685+ruppertaj@users.noreply.github.com>
2024-06-24 19:51:55 -0400

    Update Filtering_2.sh

--- CCTC/NET/Filtering_2.sh
+++ CCTC/NET/Filtering_2.sh
@@ -40,10 +40,10 @@ sudo iptables -P OUTPUT DROP
 sudo iptables -P FORWARD DROP
 
 # Allow ping (ICMP) requests (and reply) to and from the Pivot.
-sudo iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
-sudo iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT
-sudo iptables -A OUTPUT -p icmp --icmp-type 8 -j ACCEPT
-sudo iptables -A OUTPUT -p icmp --icmp-type 0 -j ACCEPT
+sudo iptables -A INPUT -s 10.10.0.40 -p icmp --icmp-type 0 -j ACCEPT
+sudo iptables -A INPUT -s 10.10.0.40 -p icmp --icmp-type 8 -j ACCEPT
+sudo iptables -A OUTPUT -d 10.10.0.40 -p icmp --icmp-type 8 -j ACCEPT
+sudo iptables -A OUTPUT -d 10.10.0.40 -p icmp --icmp-type 0 -j ACCEPT
 
 # Allow ports 6579 and 4444 for both udp and tcp traffic
 sudo iptables -A INPUT -p tcp -m multiport --ports 4444,6579 -j ACCEPT
@@ -59,3 +59,31 @@ sudo iptables -A OUTPUT -p udp -m multiport --ports 4444,6579 -j ACCEPT
 nc -l -p 9001
 -------------------------------------------------------
 
+# Allow New and Established traffic to/from via SSH, TELNET, and RDP
+sudo iptables -A INPUT -p tcp -m multiport --ports 22,23,80,3389 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -m multiport --ports 22,23,80,3389 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+# Change the Default Policy in the Filter Table for the INPUT, OUTPUT, and FORWARD chains to DROP
+sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT DROP
+sudo iptables -P FORWARD DROP
+
+# Allow New and Established traffic to/from via HTTP
+
+---------------------------------------------------
+
+NFT
+# Create input and output base chains with:
+  # Hooks
+  # Priority of 0
+  # Policy as Accept
+
+# Allow New and Established traffic to/from via SSH, TELNET, and RDP
+
+# Change your chains to now have a policy of Drop
+
+# Allow ping (ICMP) requests (and reply) to and from the Pivot.
+
+# Allow ports 5050 and 5150 for both udp and tcp traffic to/from
+
+# Allow New and Established traffic to/from via HTTP
